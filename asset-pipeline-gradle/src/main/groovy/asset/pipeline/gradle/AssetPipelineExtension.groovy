f44fc62af9c14f90537a47a471b8e94cc38b038f
Michael Yan <rain@rainboyan.com>
2023-06-01 23:16:00 +0800

    Fixes error: Could not generate a decorated class for type AssetCompile
    Duplicate annotation for class: interface org.gradle.api.tasks.Input: @org.gradle.api.tasks.Input()

--- asset-pipeline-gradle/src/main/groovy/asset/pipeline/gradle/AssetPipelineExtension.groovy
+++ asset-pipeline-gradle/src/main/groovy/asset/pipeline/gradle/AssetPipelineExtension.groovy
@@ -1,5 +1,5 @@
 /*
-* Copyright 2014 the original author or authors.
+* Copyright 2014-2023 the original author or authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,9 +16,6 @@
 
 package asset.pipeline.gradle
 
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Optional
-
 /**
  * Allows configuration of the Gradle plugin
  *
@@ -26,72 +23,62 @@ import org.gradle.api.tasks.Optional
  * @author Graeme Rocher
  */
 interface AssetPipelineExtension {
-    @Input
+
     boolean getMinifyJs()
     void setMinifyJs(boolean value)
-    @Input
+
     boolean getEnableSourceMaps()
     void setEnableSourceMaps(boolean value)
-    @Input
+
     boolean getMinifyCss()
     void setMinifyCss(boolean value)
-    @Input
+
     boolean getEnableDigests()
     void setEnableDigests(boolean value)
-    @Input
+
     boolean getSkipNonDigests()
     void setSkipNonDigests(boolean value)
-    @Input
+
     boolean getEnableGzip()
     void setEnableGzip(boolean value)
-    @Input
+
     boolean getPackagePlugin()
     void setPackagePlugin(boolean value)
-    @Input
+
     boolean getDevelopmentRuntime()
     void setDevelopmentRuntime(boolean value)
-    @Input
+
     boolean getVerbose()
     void setVerbose(boolean value)
-    @Input
-    @Optional
+
     Integer getMaxThreads()
     void setMaxThreads(Integer value)
-    @Input
-    @Optional
+
     String getCompileDir()
     void setCompileDir(String value)
-    @Input
-    @Optional
+
     String getAssetsPath()
     void setAssetsPath(String value)
-    @Input
-    @Optional
+
 	String getJarTaskName()
     void setJarTaskName(String value)
-    @Input
-    @Optional
+
     Map getMinifyOptions()
     void setMinifyOptions(Map value)
-    @Input
-    @Optional
+
     Map getConfigOptions()
     void setConfigOptions(Map value)
 
-    @Input
-    @Optional
+
     List getExcludesGzip()
     void setExcludesGzip(List value)
-    @Input
-    @Optional
+
     List getExcludes()
     void setExcludes(List value)
-    @Input
-    @Optional
+
     List getIncludes()
     void setIncludes(List value)
-    @Input
-    @Optional
+
     List<String> getResolvers()
     void setResolvers(List<String> value)
 
